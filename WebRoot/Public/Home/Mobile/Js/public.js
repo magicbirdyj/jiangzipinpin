298621975fabda2d2b0ba83e00671c1925179094
yangjing <12493085@qq.com>
2016-08-17 16:51:58 +0800

    测试微信关注

--- WebRoot/Public/Home/Mobile/Js/public.js
+++ WebRoot/Public/Home/Mobile/Js/public.js
@@ -1,21 +1,13 @@
 //如果guanzhu=weiguanzhu 弹出关注二维码对话框
 
 
-$('#dandu_buy').bind('click',function(){
-    tanchuguanzhu();
-});
-$('#kaituan_buy').bind('click',function(){
-    tanchuguanzhu();
-});
-
 
 function tanchuguanzhu(){
-    if(guanzhu==='weiguanzhu'){
+    
         showOverlay('guanzhu');
         $('#guanzhu').css('top',($(window).height()-$('#guanzhu').height())/2);
         $('.tb_zhiwen').css('height',$('.tb_zhiwen').css('width'));
         $('.tb_zhiwen').css('line-height',$('.tb_zhiwen').css('width'));
         $('.erweima_div').css('height',$('.erweima_div').css('width'));
-    }
 }
 
