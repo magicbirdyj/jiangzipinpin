4fdb16a7f3459506b5220b176db44d1c95e37095
yangjing <12493085@qq.com>
2016-08-21 19:12:02 +0800

    测试

--- Application/Home/Controller/WeixinController.class.php
+++ Application/Home/Controller/WeixinController.class.php
@@ -108,14 +108,11 @@ class WeixinController extends FontEndController {
 		</xml>";         
         $hui_msgType = "news";
         $articleCount=1;//图文消息的条数
-        //$user_name=$this->get_user($object->FromUserName);
-        $user_name=$this->get_user('oSI43woDNwqw6b_jBLpM2wPjFn_M');
+        $user_name=$this->get_user($object->FromUserName);
         $title =$user_name. "，酱紫终于等到你，点击继续购买";
         $goods=$this->get_goods_infor();
         $description=$goods['goods_name'].'[ 团购价：&yen;'.$goods['tuan_price'].']，点击继续拼团';
-        //$resultStr = sprintf($textTpl, $object->fromUsername, $object->toUsername, $time, $hui_msgType, $articleCount,$title,$description,$goods['goods_img'],$goods['url']);
-        $resultStr = sprintf($textTpl, 'oSI43woDNwqw6b_jBLpM2wPjFn_M', 'asfasdfs31224', $time, $hui_msgType, $articleCount,$title,$description,$goods['goods_img'],$goods['url']);
-        var_dump($resultStr);
+        $resultStr = sprintf($textTpl, $object->fromUsername, $object->toUsername, $time, $hui_msgType, $articleCount,$title,$description,$goods['goods_img'],$goods['url']);
         return $resultStr;
     }
     
