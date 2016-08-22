9371534f4baff55dc117eae365e89b239c88139c
yangjing <12493085@qq.com>
2016-08-22 17:26:58 +0800

    找问题

--- Application/Home/Controller/GoodsController.class.php
+++ Application/Home/Controller/GoodsController.class.php
@@ -644,8 +644,9 @@ class GoodsController extends FontEndController {
     private function alipay($order_id) {
         //$order_id=$_GET['order_id'];
         $ordermodel = D('Order');
+        var_dump("order_id=$order_id and deleted=0 ");exit;
         $order = $ordermodel->where("order_id=$order_id and deleted=0 ")->find();
-        exit;
+        
         //微信
         $user_id=$order['user_id'];
         $usersmodel=D('Users');
