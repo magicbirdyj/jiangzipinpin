1d024dd3c63f41a95de44b0dfbaa4c310a772509
yangjing <12493085@qq.com>
2016-08-23 15:38:08 +0800

    测试

--- Application/Home/Controller/FontEndController.class.php
+++ Application/Home/Controller/FontEndController.class.php
@@ -10,7 +10,7 @@ class FontEndController extends Controller {
         parent::__construct();
         
         header("content-type:text/html;charset=utf-8"); 
-        
+        var_dump($_SESSION['guanzhu']);
         //判断是否需要记录当前url 数组内必须首字母大写
         $noref=array('Index/search_m','Index/search','Goods/page','Index/menu','Order/yanzheng_zfmm','Order/queren_success','Goods/zhifu','Goods/pinglun','Member/cart_del','Member/goods_del','Goods/jiance_pay','Goods/getUniqueOrderNo','Goods/notifyweixin','Goods/notify','Goods/gmcg_wx','Goods/sellection_join','Buy/getQRPHP','Member/xiugai_zhifumima','Member/xiugai_zhifumima_check','Member/xiugai_zhifumima_success','Member/xiugai_mima','Member/xiugai_mima_check','Member/xiugai_mima_success','Member/getCode','Index/get_new_order','Goods/refund');
         $noref_contorller=array('Zhuce','Login','Weixin','Crontab');
