d6c1c345c3b42900370b2e590e397a7dd90fff67
yangjing <12493085@qq.com>
2016-08-27 22:30:15 +0800

    测试地址添加

---
+++ Application/Home/Controller/AjaxnologinController.php
@@ -0,0 +1,12 @@
+<?php
+namespace Home\Controller;
+use  Home\Controller;
+class AjaxnologinController extends FontEndController {
+    public function auto_tiaozhuan() {
+        session('auto_ref','yes');
+    }
+    
+    
+}
+
+
