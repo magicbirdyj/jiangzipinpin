b5b2029e19fefae413cb236cb764dbda5053e2a0
yangjing <12493085@qq.com>
2016-08-28 17:52:34 +0800

    分类完善

--- WebRoot/Public/Home/Mobile/Js/shop.js
+++ WebRoot/Public/Home/Mobile/Js/shop.js
@@ -1,13 +1,37 @@
-$('.tb_xianshi').bind('click',function(){
 
-    fengge_click($(this));
+if(!fengge){
+    fengge_0();
+}else if(fengge==1){
+    fengge_1()
+}
 
-});
 
 
-function fengge_click(obj){
-    if(obj.attr('id')==='fengge_1'){
-        //添加css文件
+function fengge_0(){
+    //添加css文件
+        $("<link>").attr({ rel: "stylesheet",
+            type: "text/css",
+            href: shop_1_href,
+            id:'shop_1'
+        }).appendTo("head");
+        //删除原 shop_2的css文件
+        $('#shop_2').remove();
+       //风格1的shop的偶数取消左边框
+        var data={
+            'border-left' :'none'
+        };
+        $('.shopping:odd').css(data);
+
+        //添加.goods.title的class----border-bottom
+        $('.goods_title').addClass('border-bottom');
+        //url改变
+        $('#fengge').attr('href',fengge1_url);
+        //图标变成风格2
+        obj.html('&#xe630;');
+}
+
+function fengge_1(){
+    //添加css文件
         $("<link>").attr({ rel: "stylesheet",
             type: "text/css",
             href: shop_2_href,
@@ -24,32 +48,8 @@ function fengge_click(obj){
 
         //取消.goods.title的class----border-bottom
         $('.goods_title').removeClass('border-bottom');
-        //id变成fengge_2
-        obj.attr('id','fengge_2');
+        //url改变
+        $('#fengge').attr('href',fengge0_url);
         //图标变成风格2
         obj.html('&#xe631;');
-    }else{
-        //添加css文件
-        $("<link>").attr({ rel: "stylesheet",
-            type: "text/css",
-            href: shop_1_href,
-            id:'shop_1'
-        }).appendTo("head");
-        //删除原 shop_2的css文件
-        $('#shop_2').remove();
-       //风格1的shop的偶数取消左边框
-        var data={
-            'border-left' :'none'
-        };
-        $('.shopping:odd').css(data);
-
-        //添加.goods.title的class----border-bottom
-        $('.goods_title').addClass('border-bottom');
-        //id变成fengge_1
-        obj.attr('id','fengge_1');
-        //图标变成风格2
-        obj.html('&#xe630;');
-    }
-     
 }
-
