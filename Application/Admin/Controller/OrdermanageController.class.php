e6ecbfb8a2274a00767925fc7945c52d8f4b494f
yangjing <12493085@qq.com>
2016-08-30 10:20:51 +0800

    加入统计代码

--- Application/Admin/Controller/OrdermanageController.class.php
+++ Application/Admin/Controller/OrdermanageController.class.php
@@ -5,11 +5,39 @@ class OrdermanageController extends FontEndController {
    
     public function index(){
         $ordermodel=D('Order');
-        $count=$ordermodel->where("deleted=0")->count();
-        $page=$this->get_page($count, 10);
-        $page_foot=$page->show();//显示页脚信息
-        $list=$ordermodel->table('m_order t1,m_users t2')->where("t1.user_id=t2.user_id and t1.deleted=0")->limit($page->firstRow.','.$page->listRows)->order('t1.order_id desc')->select();
-        
+        $status=$_GET['status'];
+        if($status=='0'){
+            $count=$ordermodel->where("pay_status=0 and deleted=0")->count();
+            $page=$this->get_page($count, 10);
+            $page_foot=$page->show();//显示页脚信息
+            $list=$ordermodel->table('m_order t1,m_users t2')->where("t1.pay_status=0 and t1.user_id=t2.user_id and t1.deleted=0")->limit($page->firstRow.','.$page->listRows)->order('t1.order_id desc')->select();
+        }elseif($status=='1'){
+            $count=$ordermodel->where("pay_status=0 and status=1 and deleted=0")->count();
+            $page=$this->get_page($count, 10);
+            $page_foot=$page->show();//显示页脚信息
+            $list=$ordermodel->table('m_order t1,m_users t2')->where("t1.status=1 and t1.pay_status=0 and t1.user_id=t2.user_id and t1.deleted=0")->limit($page->firstRow.','.$page->listRows)->order('t1.order_id desc')->select();
+        }elseif($status=='2'){
+            $count=$ordermodel->where("status=2 and deleted=0")->count();
+            $page=$this->get_page($count, 10);
+            $page_foot=$page->show();//显示页脚信息
+            $list=$ordermodel->table('m_order t1,m_users t2')->where("t1.status=2 and t1.user_id=t2.user_id and t1.deleted=0")->limit($page->firstRow.','.$page->listRows)->order('t1.order_id desc')->select();
+        }elseif($status=='3'){
+            $count=$ordermodel->where("status=3 and deleted=0")->count();
+            $page=$this->get_page($count, 10);
+            $page_foot=$page->show();//显示页脚信息
+            $list=$ordermodel->table('m_order t1,m_users t2')->where("t1.status=3 and t1.user_id=t2.user_id and t1.deleted=0")->limit($page->firstRow.','.$page->listRows)->order('t1.order_id desc')->select();
+        }elseif($status=='4'){
+            $count=$ordermodel->where("status=4 and deleted=0")->count();
+            $page=$this->get_page($count, 10);
+            $page_foot=$page->show();//显示页脚信息
+            $list=$ordermodel->table('m_order t1,m_users t2')->where("t1.status=4 and t1.user_id=t2.user_id and t1.deleted=0")->limit($page->firstRow.','.$page->listRows)->order('t1.order_id desc')->select();
+        }elseif(!$status){
+            $count=$ordermodel->where("deleted=0")->count();
+            $page=$this->get_page($count, 10);
+            $page_foot=$page->show();//显示页脚信息
+            $list=$ordermodel->table('m_order t1,m_users t2')->where("t1.user_id=t2.user_id and t1.deleted=0")->limit($page->firstRow.','.$page->listRows)->order('t1.order_id desc')->select();
+        }
+        $this->assign('status',$status);
         $this->assign('list',$list);
         $this->assign('page_foot',$page_foot);
         $this->display();
