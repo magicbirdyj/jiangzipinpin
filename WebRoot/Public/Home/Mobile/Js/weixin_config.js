fa96ba8e0b7b54aa3926f66f8248e57982d4ce45
yangjing <12493085@qq.com>
2016-09-04 12:33:07 +0800

    商品页面分享

--- WebRoot/Public/Home/Mobile/Js/weixin_config.js
+++ WebRoot/Public/Home/Mobile/Js/weixin_config.js
@@ -2,12 +2,13 @@
 var img=$('#lunbo_div img:first').attr('src');
     var title=$('.titile').html();
     var price=$('.price_strong').html();
-    var desc='赶紧拉朋友一起拼团吧：'+$('#title1_1').html();
+    var yuan_price=$('.yuan_price').html();
+    var desc=yuan_price+','+tuan_number+'人团价'+price+'元：'+$('#title1_1').html();
     var is_huodong=$('#huodong_guize').html();
     if(is_huodong){
         fenxiang_title='真的只需要'+price+'元：'+title;
     }else{
-        fenxiang_title= '【酱紫拼拼】：'+price+'元 '+title;
+        fenxiang_title= '【酱紫拼拼】'+tuan_number+'人团：'+price+'元 '+title;
     }
     wx.ready(function () {
         wx.onMenuShareTimeline({
