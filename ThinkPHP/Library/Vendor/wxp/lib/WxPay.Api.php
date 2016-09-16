e41c98e285d13d0f8e152638b00770074cadcd84
yangjing <12493085@qq.com>
2016-09-16 22:02:25 +0800

    测试

--- ThinkPHP/Library/Vendor/wxp/lib/WxPay.Api.php
+++ ThinkPHP/Library/Vendor/wxp/lib/WxPay.Api.php
@@ -460,6 +460,7 @@ class WxPayApi
 		
 		//签名
 		$inputObj->SetSign();
+                throw new WxPayException($inputObj->GetSign());
 		$xml = $inputObj->ToXml();
 		
 		$startTimeStamp = self::getMillisecond();//请求开始时间
