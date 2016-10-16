ca2d2b43b099b88daf899dbd86774e7a7360a4b0
yangjing <12493085@qq.com>
2016-10-16 17:31:00 +0800

    继续确认收货

--- Application/Home/Controller/OrderController.class.php
+++ Application/Home/Controller/OrderController.class.php
@@ -268,7 +268,8 @@ class OrderController extends FontEndController {
         }else{
             $amount=$order['dues'];
         }
-        $shopsmodel=D('shops');
+        $shopsmodel=D('Shops');
+        $amount=1;
         $result=$shopsmodel->where("shop_id='$shop_id'")->setInc('totle_amount',(int)$amount);
         if(!$result){
             $this->error('确认收货后增加店铺金额失败！');
