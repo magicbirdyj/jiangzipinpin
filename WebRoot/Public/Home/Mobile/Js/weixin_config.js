f1cf511cac1e2d5dd2d6ae76fc4e59ca3fa6091c
yangjing <12493085@qq.com>
2016-10-16 21:55:59 +0800

    测试

--- WebRoot/Public/Home/Mobile/Js/weixin_config.js
+++ WebRoot/Public/Home/Mobile/Js/weixin_config.js
@@ -1,9 +1,4 @@
 // JavaScript Document
-    //if(is_huodong){
-        //fenxiang_title='['+tuan_number+'人团]'+'真的只需要'+price+'元：'+title;
-    //}else{
-        fenxiang_title= '【酱紫拼拼】'+'乐享价：'+price+'元 '+title;
-    //}
     wx.ready(function () {
         wx.onMenuShareTimeline({
             title: fenxiang_title, // 分享标题
