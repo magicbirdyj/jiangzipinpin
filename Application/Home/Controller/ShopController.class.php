49fc1d290597e183a7102febf46d744f40763a7d
yangjing <12493085@qq.com>
2016-10-16 20:53:22 +0800

    店铺

--- Application/Home/Controller/ShopController.class.php
+++ Application/Home/Controller/ShopController.class.php
@@ -51,17 +51,17 @@ class ShopController extends FontEndController {
         //排序
         $order=$_GET['order'];
         if(empty($order)){ 
-            $list=$goodsmodel->where("shop_id='$shop_id' and is_delete=0")->field('price,tuan_number,goods_img_qita,goods_id,shop_name,goods_name,tuan_price,yuan_price,goods_img,comment_number,score,buy_number,daijinquan')->order('daijinquan desc,buy_number desc,score desc,last_update desc')->select();
+            $list=$goodsmodel->where("shop_id='$shop_id' and is_delete=0")->order('daijinquan desc,buy_number desc,score desc,last_update desc')->select();
         }elseif($order==='number_desc'){
-            $list=$goodsmodel->where("shop_id='$shop_id' and is_delete=0")->field('price,tuan_number,goods_img_qita,goods_id,shop_name,goods_name,tuan_price,yuan_price,goods_img,comment_number,score,buy_number,daijinquan')->order('buy_number desc,last_update desc')->select();
+            $list=$goodsmodel->where("shop_id='$shop_id' and is_delete=0")->order('buy_number desc,last_update desc')->select();
         }elseif($order==='price_desc'){
-            $list=$goodsmodel->where("shop_id='$shop_id' and is_delete=0")->field('price,tuan_number,goods_img_qita,goods_id,shop_name,goods_name,tuan_price,yuan_price,goods_img,comment_number,score,buy_number,daijinquan')->order('tuan_price desc,last_update desc')->select();
+            $list=$goodsmodel->where("shop_id='$shop_id' and is_delete=0")->order('tuan_price desc,last_update desc')->select();
         }elseif($order==='pinglun_desc'){
-            $list=$goodsmodel->where("shop_id='$shop_id' and is_delete=0")->field('price,tuan_number,goods_img_qita,goods_id,shop_name,goods_name,tuan_price,yuan_price,goods_img,comment_number,score,buy_number,daijinquan')->order('score desc,last_update desc')->select();
+            $list=$goodsmodel->where("shop_id='$shop_id' and is_delete=0")->order('score desc,last_update desc')->select();
         }elseif($order==='update_desc'){
-            $list=$goodsmodel->where("shop_id='$shop_id' and is_delete=0")->field('price,tuan_number,goods_img_qita,goods_id,shop_name,goods_name,tuan_price,yuan_price,goods_img,comment_number,score,buy_number,daijinquan')->order('last_update desc')->select();
+            $list=$goodsmodel->where("shop_id='$shop_id' and is_delete=0")->order('last_update desc')->select();
         }elseif($order==='price_asc'){
-            $list=$goodsmodel->where("shop_id='$shop_id' and is_delete=0")->field('price,tuan_number,goods_img_qita,goods_id,shop_name,goods_name,tuan_price,yuan_price,goods_img,comment_number,score,buy_number,daijinquan')->order('tuan_price,last_update desc')->select();
+            $list=$goodsmodel->where("shop_id='$shop_id' and is_delete=0")->order('tuan_price,last_update desc')->select();
         }
          $this->get_thumb($list);
         $this->assign('list',$list);
